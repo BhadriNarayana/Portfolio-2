1c452b7d1b528af5a9759a48494e9fd4169db352
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-12-18 00:16:58 +0530

    Updated portfolio

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('.fa-bars').addEventListener('click', () => {
+  document.querySelector('.sideLinks').classList.toggle('sideLinksGo')
+})
+
+document.querySelector('#cross').addEventListener('click', () => {
+  document.querySelector('.sideLinks').classList.toggle('sideLinksGo')
+})
+
+
